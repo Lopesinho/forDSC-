aa660f0c70f9edd522fcb352172080c3afb82efd
Italo Monteiro <114688809+Lopesinho@users.noreply.github.com>
2023-11-30 21:35:31 -0300

    Add files via upload

---
+++ DS-CSHARP/DS-C#/ProjetoEstoqueDesktop/Views/CadastroUnidEstoque.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace ProjetoEstoqueDesktop.Views
+{
+    partial class CadastroUnidEstoque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkOrange;
+            label1.Location = new Point(25, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(257, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Cadastro de Unidade de Estoque";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Gainsboro;
+            pictureBox1.Location = new Point(25, 57);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(202, 441);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Location = new Point(25, 57);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(900, 441);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Gainsboro;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(90, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Pesquisar";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 130);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "CNPJ/Nome";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(256, 130);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(256, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(256, 182);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(256, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(256, 241);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(256, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(259, 305);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(253, 23);
+            textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(259, 363);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(253, 23);
+            textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(570, 157);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(146, 23);
+            textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(570, 209);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(146, 23);
+            textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(570, 256);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(146, 23);
+            textBox9.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(570, 305);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(146, 23);
+            textBox10.TabIndex = 13;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(793, 157);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(96, 23);
+            textBox11.TabIndex = 14;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(256, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 17);
+            label3.TabIndex = 15;
+            label3.Text = "Proprietário:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(256, 163);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 17);
+            label4.TabIndex = 16;
+            label4.Text = "Rua:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.White;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(256, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 17);
+            label5.TabIndex = 17;
+            label5.Text = "Cidade:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.White;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(259, 285);
+            label6.Name = "label6";
+            label6.Size = new Size(71, 17);
+            label6.TabIndex = 18;
+            label6.Text = "Telefones:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(259, 343);
+            label7.Name = "label7";
+            label7.Size = new Size(84, 17);
+            label7.TabIndex = 19;
+            label7.Text = "Encarregado";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.White;
+            label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(570, 137);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 17);
+            label8.TabIndex = 20;
+            label8.Text = "Num:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(570, 189);
+            label9.Name = "label9";
+            label9.Size = new Size(35, 17);
+            label9.TabIndex = 21;
+            label9.Text = "CEP:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(570, 236);
+            label10.Name = "label10";
+            label10.Size = new Size(31, 17);
+            label10.TabIndex = 22;
+            label10.Text = "m2:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.White;
+            label11.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(759, 158);
+            label11.Name = "label11";
+            label11.Size = new Size(28, 17);
+            label11.TabIndex = 23;
+            label11.Text = "UF:";
+            label11.Click += label11_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.White;
+            label12.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(570, 285);
+            label12.Name = "label12";
+            label12.Size = new Size(31, 17);
+            label12.TabIndex = 24;
+            label12.Text = "m3:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.ForeColor = Color.DarkOrange;
+            label13.Location = new Point(711, 91);
+            label13.Name = "label13";
+            label13.Size = new Size(76, 30);
+            label13.TabIndex = 25;
+            label13.Text = "ATIVO";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Orange;
+            button1.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(625, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 44);
+            button1.TabIndex = 26;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Orange;
+            button2.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(759, 396);
+            button2.Name = "button2";
+            button2.Size = new Size(91, 44);
+            button2.TabIndex = 27;
+            button2.Text = "Inativar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // CadastroUnidEstoque
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(957, 526);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Name = "CadastroUnidEstoque";
+            Text = "Form1";
+            Load += CadastroUnidEstoque_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ DS-CSHARP/DS-C#/ProjetoEstoqueDesktop/Views/Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Views
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.WhiteSmoke;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(437, 284);
+            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 30);
+            button1.TabIndex = 0;
+            button1.Text = "ENTRAR";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(373, 200);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(210, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(372, 245);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(210, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Light", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(373, 186);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 13);
+            label1.TabIndex = 3;
+            label1.Text = "Insira o nome do Usuário:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Light", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(372, 230);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(123, 13);
+            label2.TabIndex = 4;
+            label2.Text = "Insira a senha do Usuário:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Corbel", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Orange;
+            label3.Location = new Point(441, 149);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 27);
+            label3.TabIndex = 5;
+            label3.Text = "LOGIN";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(950, 485);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Login";
+            Tag = "";
+            Text = "Form1";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
